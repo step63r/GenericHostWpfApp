77b3a9a452d20389d5de6dc5a9c19759d0c7dcd2
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2024-06-29 01:12:31 +0900

    プロジェクト ファイルを追加します。

---
+++ GenericHostWpfApp/App.xaml.cs
@@ -0,0 +1,74 @@
+﻿using GenericHostWpfApp.Services;
+using GenericHostWpfApp.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace GenericHostWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        /// <summary>
+        /// The.NET Generic Host provides dependency injection, configuration, logging, and other services.
+        /// </summary>
+        private static readonly IHost _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureAppConfiguration((config) =>
+            {
+                config.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location));
+            })
+            .ConfigureServices((context, services) =>
+            {
+                // App Host
+                services.AddHostedService<ApplicationHostService>();
+
+                // MainWindow
+                services.AddScoped<MainWindow>();
+            })
+            .Build();
+
+        /// <summary>
+        /// Gets registered service.
+        /// </summary>
+        /// <typeparam name="T">Type of the service to get.</typeparam>
+        /// <returns>Instance of the service or <see langword="null"/>.</returns>
+        public static T GetService<T>()
+            where T : class
+        {
+            return _host.Services.GetService(typeof(T)) as T;
+        }
+
+        /// <summary>
+        /// Occurs when the application is loading.
+        /// </summary>
+        private async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await _host.StartAsync();
+        }
+
+        /// <summary>
+        /// Occurs when the application is closing.
+        /// </summary>
+        private async void OnExit(object sender, ExitEventArgs e)
+        {
+            await _host.StopAsync();
+
+            _host.Dispose();
+        }
+
+        /// <summary>
+        /// Occurs when an exception is thrown by an application but not handled.
+        /// </summary>
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
+        }
+    }
+}
---
+++ GenericHostWpfApp/Services/ApplicationHostService.cs
@@ -0,0 +1,70 @@
+﻿using GenericHostWpfApp.Windows;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace GenericHostWpfApp.Services
+{
+    /// <summary>
+    /// アプリケーションのマネージドホスト
+    /// </summary>
+    /// <param name="applicationLifetime">IHostApplicationLifetime</param>
+    /// <param name="serviceProvider">IServiceProvider</param>
+    internal class ApplicationHostService(IHostApplicationLifetime applicationLifetime, IServiceProvider serviceProvider) : IHostedService
+    {
+        #region インターフェイス
+        /// <summary>
+        /// IHostApplicationLifetime
+        /// </summary>
+        private readonly IHostApplicationLifetime _applicationLifetime = applicationLifetime;
+
+        /// <summary>
+        /// IServiceProvider
+        /// </summary>
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
+        #endregion
+
+        #region メンバ変数
+        /// <summary>
+        /// Window
+        /// </summary>
+        private Window _window;
+        #endregion
+
+        #region IHostedService
+        /// <summary>
+        /// サービスが開始するときにトリガーされるメソッド
+        /// </summary>
+        /// <param name="cancellationToken">キャンセルトークン</param>
+        /// <returns>実行結果</returns>
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await HandleActivateAsync();
+        }
+
+        /// <summary>
+        /// サービスが終了するときにトリガーされるメソッド
+        /// </summary>
+        /// <param name="cancellationToken">キャンセルトークン</param>
+        /// <returns>実行結果</returns>
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+        #endregion
+
+        /// <summary>
+        /// HandleActivateAsync
+        /// </summary>
+        /// <returns></returns>
+        private async Task HandleActivateAsync()
+        {
+            if (!Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                _window = (_serviceProvider.GetService(typeof(MainWindow)) as MainWindow)!;
+                _window!.Show();
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
